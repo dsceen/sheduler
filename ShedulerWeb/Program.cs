9dbc512842325cb7a9e64596a1c7c6d5481cffac
scn <d.sceen@gmail.com>
2018-08-30 20:16:41 +0300

    add windows and unix config files

--- ShedulerWeb/Program.cs
+++ ShedulerWeb/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.Runtime.InteropServices;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 
 namespace ShedulerWeb
@@ -13,6 +15,26 @@ namespace ShedulerWeb
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseSetting("detailedErrors", "true")
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    builder.Sources.Clear();
+                    var env = context.HostingEnvironment;
+
+                    var systemType = "windows";
+
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ||
+                        RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                        systemType = "unix";
+                    
+                    builder
+                        .SetBasePath(env.ContentRootPath)
+                        .AddJsonFile("appsettings.json")
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                        .AddJsonFile($"appsettings.{systemType}.json", true);
+
+                })
+                .CaptureStartupErrors(true)
                 .UseStartup<Startup>();
     }
 }
