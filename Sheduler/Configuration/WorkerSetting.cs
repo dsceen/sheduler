1e4c824cd6e2babf977a8182e9f076a444490f27
scn <d.sceen@gmail.com>
2018-09-01 23:15:17 +0300

    add description for cron format

--- Sheduler/Configuration/WorkerSetting.cs
+++ Sheduler/Configuration/WorkerSetting.cs
@@ -4,7 +4,18 @@
     {
         public string PathToDll { get; set; }
         public string PathToConfig { get; set; }
+
+        /// <summary>
+        /// Use cron format: &lt;Minute&gt; &lt;Hour&gt; &lt;Day_of_the_Month&gt; &lt;Month_of_the_Year&gt; &lt;Day_of_the_Week&gt; &lt;Year&gt;<br/>
+        /// 
+        /// Example: <br/>
+        /// * * * * * *                         Each minute<br/>
+        ///59 23 31 12 5 *                     One minute  before the end of year if the last day of the year is Friday<br/>
+        ///59 23 31 DEC Fri *                  Same as above(different notation)<br/>
+        ///45 17 7 6 * *                       Every year, on June 7th at 17:45<br/>
+        ///</summary>
         public string StartAt { get; set; }
+
         public int RestartAfterFailCount { get; set; }
     }
 }
