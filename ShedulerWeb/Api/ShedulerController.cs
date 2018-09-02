5aa22d7e47c31995c37232bc4b3edf7ec2c34f53
scn <d.sceen@gmail.com>
2018-09-02 15:00:52 +0300

    UI update

---
+++ ShedulerWeb/Api/ShedulerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sheduler.Core.Interfaces;
+
+namespace ShedulerWeb.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShedulerController : ControllerBase
+    {
+        private readonly IDllSheduler _dllSheduler;
+
+        public ShedulerController(IDllSheduler dllSheduler)
+        {
+            _dllSheduler = dllSheduler;
+        }
+        [Route("/start")]
+        public string Start()
+        {
+            _dllSheduler.Start();
+            return "started";
+        }
+
+        [Route("/stop")]
+        public string Stop()
+        {
+            _dllSheduler.Stop();
+            return "stop";
+        }
+
+        [Route("/status")]
+        public string Status()
+        {
+            return _dllSheduler.IsStarted.ToString();
+        }
+    }
+}
---
+++ ShedulerWeb/wwwroot/js/TUpdater.js
@@ -0,0 +1,33 @@
+﻿var ArrOfTObj = [];
+
+$.fn.statusUpdater = function () {
+    this.each(function () {
+        AddToPool($(this));
+    });
+};
+
+function AddToPool(obj) {
+    ArrOfTObj.push(obj);
+}
+
+function UpdateObj() {
+    $.ajax({
+        url: "/status",
+        cache: false,
+        success: function (data) {
+            $.each(ArrOfTObj, function (key) {
+                console.log("currunt state = " + data);
+                if (data === "True")
+                    $(ArrOfTObj[key]).html("Started");
+                else
+                    $(ArrOfTObj[key]).html("Stoped");
+            });
+        },
+        error: function () {
+            console.log("error: cant recive data");
+        }
+    });
+}
+
+setInterval(UpdateObj, 1000);
+
